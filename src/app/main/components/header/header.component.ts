575d08c7b227543237e079cfe58963d202cb2161
juca <juca@jucasoft.com>
2021-04-27 12:08:44 +0200

    profilato menu

--- src/app/main/components/header/header.component.ts
+++ src/app/main/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {Observable} from 'rxjs';
+import {select, Store} from '@ngrx/store';
+import {AuthStoreSelectors} from '@root-store/auth-store';
 
 @Component({
   selector: 'app-header',
@@ -6,11 +9,10 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
     <div class="p-grid p-justify-between">
       <div class="p-col">
         <div>
-          <app-hamburger-button></app-hamburger-button>
+          <app-hamburger-button *ngIf="loggedIn$ | async as loggedIn;"></app-hamburger-button>
         </div>
       </div>
       <div class="p-col text-align-right">
-<!--        <em class="fas fa-2x fa-user fa-button p-1" style="color: #FFF;"></em>-->
         <app-login></app-login>
       </div>
     </div>
@@ -20,17 +22,21 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
       opacity: 0.5;
       transition: transform 0.2s;
       /*transform: scale(1.1);*/
-      cursor: pointer; cursor: hand;
+      cursor: pointer;
+      cursor: hand;
     }
   `],
   encapsulation: ViewEncapsulation.None
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() {
+  loggedIn$: Observable<boolean>;
+
+  constructor(private store$: Store) {
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.loggedIn$ = this.store$.pipe(select(AuthStoreSelectors.selectIsLoggedIn));
   }
 
 }
